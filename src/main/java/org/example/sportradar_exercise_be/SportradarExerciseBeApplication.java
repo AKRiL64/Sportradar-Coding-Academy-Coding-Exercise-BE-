66d92342beef33ad122a0d27904a1236cd2a6492
Kiryu Setsuna <lerroy.k@icloud.com>
2026-03-24 17:10:09 +0100

    initial commit

---
+++ src/main/java/org/example/sportradar_exercise_be/SportradarExerciseBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.sportradar_exercise_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportradarExerciseBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportradarExerciseBeApplication.class, args);
+    }
+
+}
